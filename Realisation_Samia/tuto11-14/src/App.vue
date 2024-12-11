0f970a2880b7566094edd84862d5a2562c959eef
samiaBoug <bougtoubsamia@gmail.com>
2024-12-12 00:56:35 +0100

    save

---
+++ Realisation_Samia/tuto11-14/src/App.vue
@@ -0,0 +1,34 @@
+
+<script setup>
+/*
+import { ref } from 'vue';
+import ChildComp from './ChildComp.vue'
+import ChildComp from ':';
+
+const greeting = ref('Hello from parent')
+const childMsg = ref('no child nessage yet')
+*/
+import { ref } from "vue";
+import { ChildComp } from "./ChildComp.vue";
+const msg = ref("from parent")
+</script>
+
+<template>
+  <!--
+  <ChildComp :msg="greeting" @response="(msg)=> childMsg = msg" />
+  <p>{{ childMsg }}</p>
+  -->
+  <ChildComp>
+    Message : {{ msg }}
+  </ChildComp>
+</template>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- Realisation_Samia/tuto11-15/src/ChildComp.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-defineProps({
-  msg: String
-})
-</script>
-
-<template>
-  <h2>{{ msg || 'No props passed yet' }}</h2>
-</template>
