06ce9ce6b92f2ff536d1249c316b53536fde3b2e
samiaBoug <bougtoubsamia@gmail.com>
2024-12-12 00:22:14 +0100

    save

---
+++ Realisation_Samia/tuto11-15/src/App.vue
@@ -0,0 +1,21 @@
+
+<script setup>
+import { ref } from 'vue';
+import ChildComp from './ChildComp.vue'
+
+const greeting = ref('Hello from parent')
+</script>
+
+<template>
+  <ChildComp :msg="greeting" />
+</template>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
